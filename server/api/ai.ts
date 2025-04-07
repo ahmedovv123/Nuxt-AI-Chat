e4f6f318361e2f14fdf47c87314fbf76ba8f9e0c
Ahmet Ahmedov <ahmet@hph.io>
2025-04-07 12:57:09 +0300

    add ai models

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,14 +1,27 @@
+import {
+  createOpenAIModel,
+  generateChatResponse,
+} from "../services/ai-service";
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   const { messages } = body;
 
   const id = messages.length.toString();
-  const lastMessage = messages[messages.length - 1];
+
+  const { openaiApiKey } = useRuntimeConfig();
+
+  const openaiModel = createOpenAIModel(openaiApiKey);
+
+  // TODO: Add ollama model
+  // const ollamaModel = createOllamaModel();
+
+  const response = await generateChatResponse(openaiModel, messages);
 
   return {
     id,
     role: "assistant",
-    content: `(serevr) You said: ${lastMessage.content}`,
+    content: response,
   };
 });
