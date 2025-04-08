9395935c2d52d1998d9eef7fdf9986121a89c6f1
Ahmet Ahmedov <ahmet@hph.io>
2025-04-08 15:23:38 +0300

    add md styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/mdc"],
 
   runtimeConfig: {
     openaiApiKey: "",
+  },
+
+  mdc: {
+    highlight: {
+      theme: "material-theme-palenight",
+      langs: ["html", "markdown", "vue", "typescript", "javascript"],
+    },
   },
 });
